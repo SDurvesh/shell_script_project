76fc729a34597cc46b1413a182e93f6450b9d512
Durvesh S Shendokar <121811974+SDurvesh@users.noreply.github.com>
2024-07-05 11:14:45 +0530

    Delete ram_monitroing.sh

--- ram_monitroing.sh
+++
@@ -1,9 +0,0 @@
-FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
-TH=500
-
-if [[ $FREE_SPACE -lt $TH ]]
-then
-        echo "Warning , RAM Space is running low"
-else
-        echo "RAM space is Sufficient- $FREE_SPACE M"
-fi
